80c47d29f2b1dee8b9dbe1c72d7bd9e17c2cd154
Einarr07 <aidenperse@gmail.com>
2024-11-17 17:07:04 -0500

    Agrego comentarios a los ejercicios

--- intermedio/metodos/ejercicio.js
+++ intermedio/metodos/ejercicio.js
@@ -1,50 +1,68 @@
+// Función que obtiene información de una materia específica o de todas las materias
 const obtenerInformacion = (materia) => {
+    // Objeto que contiene las materias, el profesor y los alumnos inscritos
     materias = {
         fisica: ["Perez", "Marco", "Pedro", "Hernesto", "Estefano", "Alejandro"],
         programacion: ["Rodriguez", "Marco", "Pedro", "Hernesto", "Estefano", "Julian", "Alejandro"],
         matematica: ["Hernandez", "Marco", "Pedro", "Hernesto", "Julian", "Alejandro"],
         quimica: ["Rodriguez", "Marco", "Pedro", "Hernesto", "Estefano", "Julian", "Alejandro"],
         lenguaje: ["Aguilar", "Marco", "Pedro", "Estefano", "Alejandro"],
-    }
+    };
+
+    // Si la materia solicitada existe en el objeto, retorna un array con la información
     if (materias[materia] !== undefined) {
         return [materias[materia], materia, materias];
     } else {
+        // Si no se encuentra la materia, retorna todo el objeto `materias`
         return materias;
     }
-}
+};
 
-
+// Función que muestra información de una materia específica
 const mostrarInformacion = (materia) => {
+    // Obtiene la información de la materia usando la función `obtenerInformacion`
     let informacion = obtenerInformacion(materia);
 
+    // Si se obtuvo información válida
     if (informacion !== false) {
-        let profesor = informacion[0][0];
-        let alumnos = informacion[0];
-        alumnos.shift();
-        document.write(`El profesor de <b>${informacion[1]}</b> es: <b style="color: red">${profesor}</b><br>
-    Los alumnos son: <b style="color: aqua">${alumnos}</b><br>   <br>
-`);
-    }
-}
+        let profesor = informacion[0][0]; // El primer elemento del array es el nombre del profesor
+        let alumnos = informacion[0]; // Resto del array contiene a los alumnos
+        alumnos.shift(); // Elimina al profesor del array para quedarnos solo con los alumnos
 
+        // Muestra la información en el documento
+        document.write(`El profesor de <b>${informacion[1]}</b> es: <b style="color: red">${profesor}</b><br>
+        Los alumnos son: <b style="color: aqua">${alumnos}</b><br>   <br>
+        `);
+    }
+};
+
+// Función que cuenta cuántas clases tiene un alumno y cuáles son
 const cantidadClases = (alumno) => {
-    let informacion = obtenerInformacion()
-    let clasesPresentes = [];
-    let cantidadTotal = 0;
+    // Obtiene toda la información de las materias
+    let informacion = obtenerInformacion();
+    let clasesPresentes = []; // Array para almacenar las clases en las que está el alumno
+    let cantidadTotal = 0; // Contador de clases en las que está el alumno
+
+    // Itera sobre cada materia en el objeto `informacion`
     for (info in informacion) {
+        // Si el alumno se encuentra en la lista de alumnos de esa materia
         if (informacion[info].includes(alumno)) {
-            cantidadTotal++;
-            clasesPresentes.push(" " + info )
+            cantidadTotal++; // Incrementa el contador
+            clasesPresentes.push(" " + info); // Agrega el nombre de la materia al array
         }
     }
+
+    // Retorna un mensaje con la cantidad de clases y la lista de clases en las que está el alumno
     return `<b style="color: aqua">${alumno}</b> está en <b>${cantidadTotal}</b> clases: 
-    <b style="color: green">${clasesPresentes}</b> <br>`
-}
+    <b style="color: green">${clasesPresentes}</b> <br>`;
+};
 
-mostrarInformacion("fisica")
-mostrarInformacion("programacion")
-mostrarInformacion("matematica")
-mostrarInformacion("quimica")
-mostrarInformacion("lenguaje")
+// Muestra información de cada materia específica
+mostrarInformacion("fisica");
+mostrarInformacion("programacion");
+mostrarInformacion("matematica");
+mostrarInformacion("quimica");
+mostrarInformacion("lenguaje");
 
-document.write(cantidadClases("Hernesto"))
+// Muestra la cantidad de clases en las que está inscrito un alumno específico
+document.write(cantidadClases("Hernesto"));
--- intermedio/metodos/ejercicio2.js
+++ intermedio/metodos/ejercicio2.js
@@ -1,19 +1,26 @@
+// Objeto que contiene las materias y sus listas de participantes
 let materias = {
     fisica: ["Perez", "Marco", "Pedro", "Hernesto", "Estefano", "Alejandro"],
     programacion: ["Rodriguez", "Marco", "Pedro", "Hernesto", "Estefano", "Julian", "Alejandro"],
     matematica: ["Hernandez", "Marco", "Pedro", "Hernesto", "Julian", "Alejandro"],
     quimica: ["Rodriguez", "Marco", "Pedro", "Hernesto", "Estefano", "Julian", "Alejandro"],
     lenguaje: ["Aguilar", "Marco", "Pedro", "Estefano", "Alejandro"],
-}
+};
 
-
+// Función para inscribir un alumno en una materia
 const inscribir = (alumno, materia) => {
+    // Obtiene la lista de personas en la materia seleccionada
     personas = materias[materia];
 
+    // Verifica si la materia tiene el máximo permitido de estudiantes (21)
     if (personas.length >= 21) {
-        document.write(`Lo siento <b>${alumno}</b>, las clases de <b>${materia}</b> ya estan llenas <br>`)
+        // Si la clase está llena, muestra un mensaje de error
+        document.write(`Lo siento <b>${alumno}</b>, las clases de <b>${materia}</b> ya están llenas <br>`);
     } else {
+        // Si hay cupo, agrega al alumno a la lista
         personas.push(alumno);
+
+        // Actualiza el objeto `materias` dependiendo de la materia seleccionada
         if (materia == "fisica") {
             materias = {
                 fisica: personas,
@@ -21,56 +28,59 @@ const inscribir = (alumno, materia) => {
                 matematica: materias["matematica"],
                 quimica: materias["quimica"],
                 lenguaje: materias["lenguaje"],
-            }
-        } else if ( materia == "programacion" ) {
+            };
+        } else if (materia == "programacion") {
             materias = {
                 fisica: materias["fisica"],
                 programacion: personas,
                 matematica: materias["matematica"],
                 quimica: materias["quimica"],
                 lenguaje: materias["lenguaje"],
-            }
-        }else if ( materia == "matemateica" ) {
+            };
+        } else if (materia == "matemateica") {
+            // Nota: Hay un error tipográfico en "matemateica", debe ser "matematica"
             materias = {
                 fisica: materias["fisica"],
                 programacion: materias["programacion"],
                 matematica: personas,
                 quimica: materias["quimica"],
                 lenguaje: materias["lenguaje"],
-            }
-        }else if ( materia == "quimica" ) {
+            };
+        } else if (materia == "quimica") {
             materias = {
                 fisica: materias["fisica"],
                 programacion: materias["programacion"],
                 matematica: materias["matematica"],
-                quimica:personas,
+                quimica: personas,
                 lenguaje: materias["lenguaje"],
-            }
-        }else if ( materia == "lenguaje" ) {
+            };
+        } else if (materia == "lenguaje") {
             materias = {
                 fisica: materias["fisica"],
                 programacion: materias["programacion"],
                 matematica: materias["matematica"],
                 quimica: materias["quimica"],
                 lenguaje: personas,
-            }
+            };
         }
-        document.write(`Felicidades ${alumno} te haz inscrito a ${materia} <br> <br>`)
+        // Muestra un mensaje de confirmación de inscripción
+        document.write(`Felicidades ${alumno}, te has inscrito a ${materia} <br><br>`);
     }
+};
 
-}
+// Muestra la lista inicial de alumnos en la materia de programación
+document.write("Lista de alumnos: " + materias["programacion"] + "<br><br>");
 
-document.write("Lista de alumnos: " + materias["programacion"] + "<br> <br>")
+// Realiza inscripciones en diferentes materias
+inscribir("Pedro", "quimica");
+inscribir("Norma", "lenguaje");
+inscribir("Raphaela", "programacion");
+inscribir("Andy", "lenguaje");
+inscribir("Gabriel", "programacion");
+inscribir("Nestor", "lenguaje");
+inscribir("Jorge", "matematica");
+inscribir("Violet", "matematica");
+inscribir("Lucia", "quimica");
 
-inscribir("Pedro", "quimica")
-inscribir("Norma", "lenguaje")
-inscribir("Raphaela", "programacion")
-inscribir("Andy", "lenguaje")
-inscribir("Gabriel", "programacion")
-inscribir("Nestor", "lenguaje")
-inscribir("Jorge", "matematica")
-inscribir("Violet", "matematica")
-inscribir("Lucia", "quimica")
-
-
-document.write("Lista de alumnos actualizada:<br>" + materias["programacion"])
+// Muestra la lista actualizada de alumnos en la materia de programación
+document.write("Lista de alumnos actualizada:<br>" + materias["programacion"]);
